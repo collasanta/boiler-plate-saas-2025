fccf6cfc6b0cf26792909b798a02c23ba9d94269
Victor Henrique Collasanta <96581579+collasanta@users.noreply.github.com>
2025-01-22 16:16:19 +0700

    add default next-intl transaltion to landingpage

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -1,42 +1,42 @@
 "use client";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-
-const testimonials = [
-  {
-    name: "Nutricionistas",
-    avatar: "A",
-    title: "Designer",
-    description: "Acompanhe o progresso dos seus pacientes de perto, diariamente, e de forma única. E automatize o envio de anameneses e feedbacks ",
-  },
-  {
-    name: "Pacientes",
-    avatar: "J",
-    title: "Software Engineer",
-    description: "Relate diariamente sua evolução, e receba feedbacks do seu profissional de saúde de forma mais acertiva e personalizada",
-  }
-];
+import { useTranslations } from "next-intl";
 
 export const LandingContent = () => {
+  const t = useTranslations("Homepage.landingContent");
+
+  const testimonials = [
+    {
+      key: "nutritionists",
+      avatar: "A",
+    },
+    {
+      key: "patients",
+      avatar: "J",
+    },
+  ];
+
   return (
     <div className="px-10 pb-20 bg-white">
-      <h2 className="text-center text-3xl text-white font-extrabold mb-5 text-gray-500 pt-8">Feito para:</h2>
+      <h2 className="text-center text-3xl text-white font-extrabold mb-5 text-gray-500 pt-8">{t("title")}</h2>
       <div className="grid justify-center grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-6">
         {testimonials.map((item) => (
-          <Card key={item.description} className="bg-gradient-to-r hover:animate-pulse bg-gradient-to-r from-emerald-500 via-teal-500 to-teal-400 shadow-lg border-none text-white">
+          <Card
+            key={item.key}
+            className="bg-gradient-to-r hover:animate-pulse bg-gradient-to-r from-emerald-500 via-teal-500 to-teal-400 shadow-lg border-none text-white"
+          >
             <CardHeader>
               <CardTitle className="flex items-center gap-x-2">
                 <div>
-                  <p className="text-lg">{item.name}</p>
+                  <p className="text-lg">{t(`testimonials.${item.key}.name`)}</p>
                 </div>
               </CardTitle>
-              <CardContent className="pt-4 px-0">
-                {item.description}
-              </CardContent>
+              <CardContent className="pt-4 px-0">{t(`testimonials.${item.key}.description`)}</CardContent>
             </CardHeader>
           </Card>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -1,42 +1,40 @@
 "use client";
 
-import TypewriterComponent from "typewriter-effect";
 import { Link } from "@/i18n/routing";
 import { useAuth } from "@clerk/nextjs";
+import { useTranslations } from "next-intl";
+import TypewriterComponent from "typewriter-effect";
 
 import { Button } from "@/components/ui/button";
 
 export const LandingHero = () => {
   const { isSignedIn } = useAuth();
+  const t = useTranslations("Homepage");
 
   return (
     <div className="bg-white">
       <div className="p-2 shadow-lg m-4 text-white font-bold py-5 md:py-30 text-center space-y-5 bg-secondary rounded-[40px]">
         <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold text-gray-500">
-          <h1>Fazendo da saúde </h1>
+          <h1>{t("mainTitle")}</h1>
           <div className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-200 to-emerald-600">
             <TypewriterComponent
               options={{
-                strings: ["Um hábito", "Um estilo de vida", "Uma rotina"],
+                strings: [t("typewriter.habit"), t("typewriter.lifestyle"), t("typewriter.routine")],
                 autoStart: true,
                 loop: true,
               }}
             />
           </div>
         </div>
-        <div className="text-sm md:text-xl font-medium text-zinc-400">
-          Acompanhe o resultados dos seus clientes diariamente de forma automática
-        </div>
+        <div className="text-sm md:text-xl font-medium text-zinc-400">{t("subtitle")}</div>
         <div>
           <Link href={isSignedIn ? "/app/dashboard" : "/sign-up"}>
             <Button variant="premium" className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
-              Entrar
+              {t("cta")}
             </Button>
           </Link>
         </div>
-        <div className="text-zinc-400 text-xs md:text-sm font-normal">
-          Se diferencie e Garanta melhores resultados para seus pacientes
-        </div>
+        <div className="text-zinc-400 text-xs md:text-sm font-normal">{t("bottomText")}</div>
         <div className="mx-auto">
           {/* <Image width={250} height={500} alt="gif" src="/gif.gif" className="mx-auto rounded-md" /> */}
         </div>
