32b0ceff9db7cfa247727a1af3852fd4e4fe3f5e
Victor Henrique Collasanta <96581579+collasanta@users.noreply.github.com>
2025-01-24 17:54:34 +0700

    add language switcher on navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,18 @@
 import { UserButton } from "@clerk/nextjs";
+import LocaleSwitcher from "./locale-switcher";
 import MobileSidebar from "./mobile-sidebar";
 
 const Navbar = async () => {
-    return (
-        <div className="flex items-center p-4">
-            <MobileSidebar />
-            <div className="flex w-full justify-end min-h-[33px]">
-                <UserButton afterSignOutUrl="/" />
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="flex items-center p-4">
+      <MobileSidebar />
+      <div className="flex w-full justify-end min-h-[33px] space-x-4">
+        <LocaleSwitcher />
+        <UserButton />
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
